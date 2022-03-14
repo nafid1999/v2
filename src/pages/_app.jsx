160ce7b89f26a73dc5601576b51332d374414ca4
anafid <anafid@novelis.io>
2022-03-14 14:46:50 +0000

    Feature/axios setup

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ThemeProvider } from '@emotion/react'
 import { appWithTranslation } from 'next-i18next'
+import { QueryClientProvider } from 'react-query'
+
 import getMUITheme from '../config/MUITheme'
 import useUserPreferencesStore from '../store/userPreferences'
 import Container from '../layouts'
 import '../../styles/globals.css'
+import useQueryClient from '../config/ReactQuery/queryClient'
 
 function Novy({ Component, pageProps }) {
   const { theme } = useUserPreferencesStore()
+  const queryClient = useQueryClient()
 
-  /* eslint-disable react/jsx-props-no-spreading */
   return (
-    <ThemeProvider theme={getMUITheme(theme)}>
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={getMUITheme(theme)}>
+        <Container>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
