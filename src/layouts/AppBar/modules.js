982a076cbe3f223184a76a9d60e18bdda4345033
Ilazaar <ilazaar@novelis.io>
2022-03-18 16:15:49 +0100

    Add Modules

---
+++ src/layouts/AppBar/modules.js
@@ -0,0 +1,40 @@
+export default {
+  items: [
+    {
+      name: 'recruitment',
+      url: '/recruitment',
+      permissions: 'recruitment.manageRecruitment',
+    },
+    {
+      name: 'collaborators',
+      url: '/collaborators',
+      permissions: 'collaborators.manageCollaborators',
+    },
+    {
+      name: 'suppliers_clients',
+      url: '/suppliers',
+      permissions: 'suppliers.manageSuppliers',
+    },
+    {
+      name: 'crm',
+      url: '/crm',
+      permissions: 'crm.manageCrm',
+    },
+    {
+      name: 'pay',
+      url: '/pay',
+      permissions: 'pay.managePay',
+    },
+    {
+      name: 'administration',
+      url: '/administration',
+      permissions: 'administration.manageAdministration',
+    },
+    {
+      name: 'pilotage',
+      url: process.env.NEXT_PUBLIC_PILOTAGE_URL,
+      icon: 'fas fa-chart-line',
+      permissions: 'pilotage.managePilotage',
+    },
+  ],
+}
